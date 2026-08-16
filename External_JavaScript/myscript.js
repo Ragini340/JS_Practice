3ee8e27c8f63e5517ccf9e643028254330953ac6
Ragini340 <ragini31bxr@gmail.com>
2026-08-16 14:14:11 +0530

    #JS_Practice Added external JavaScript implementation example

---
+++ External_JavaScript/myscript.js
@@ -0,0 +1,11 @@
+﻿/*
+    External JavaScript Example
+*/
+
+document.writeln("Hello World");
+
+document.writeln("Message from External Script");
+
+for (let i = 1; i <= 10; i++) {
+    document.writeln(i + "<br>");
+}
